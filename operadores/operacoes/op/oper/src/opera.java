a17e205365cbd1057259e8911b4ed80b9b0a0fde
Gustavo <gustavoundericed@gmail.com>
2024-09-18 21:44:28 -0300

    operacao divisao

---
+++ operadores/operacoes/op/oper/src/opera.java
@@ -0,0 +1,7 @@
+public class opera {
+    public static void main(String[] args) throws Exception {
+       int x = 10;
+       double y = x /3 ;
+       System.out.println("o resultado e: " + y); 
+    }
+}
