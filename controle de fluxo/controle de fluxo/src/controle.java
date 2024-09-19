76b843191a03c8a51fefe183e6b2b62e398a1262
Gustavo <gustavoundericed@gmail.com>
2024-09-18 22:19:30 -0300

    composta1

--- controle de fluxo/controle de fluxo/src/controle.java
+++ controle de fluxo/controle de fluxo/src/controle.java
@@ -1,11 +1,14 @@
 public class controle {
     public static void main(String[] args) throws Exception {
       double saldo =25.0;
-      double valorSolicitado = 17.0;
+      double valorSolicitado = 28.0;
       
       if (valorSolicitado < saldo)
         saldo =saldo -valorSolicitado;
+       
 
-        System.out.println(saldo);
+        else
+       System.out.println("saldo insuficiente! " + saldo);
+       System.out.println(saldo);
     }
 }
--- controle de fluxo/controle estrutura composta/fluxo composto/src/estruturacomposta.java
+++ controle de fluxo/controle estrutura composta/fluxo composto/src/estruturacomposta.java
@@ -1,6 +1,6 @@
 public class estruturacomposta {
     public static void main(String[] args) throws Exception {
-        int nota =6;
+        int nota =9;
          if (nota >= 7)
          System.out.println("Aprovado");
 
