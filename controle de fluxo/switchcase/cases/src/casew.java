c31eb8bf15d534d4b0f4ee2e6d82a43c35de4391
Gustavo <gustavoundericed@gmail.com>
2024-09-18 22:54:32 -0300

    swicth case

---
+++ controle de fluxo/switchcase/cases/src/casew.java
@@ -0,0 +1,22 @@
+public class casew{ 
+    public static void main(String[] args) throws Exception {
+        String sigla ="M";
+        switch (sigla) {
+            case  "P":{
+                System.out.println("Pequeno");
+                break;
+            }
+            case  "M":{
+                System.out.println("Médio");
+                break;
+             } 
+                case  "G":{
+                    System.out.println("Grande");
+                    break;
+             }  
+               
+            default:
+                throw new AssertionError();
+   }
+    }
+}
