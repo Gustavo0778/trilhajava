1965129a6beae842dc96eb191b7849c202a14d54
Gustavo <gustavoundericed@gmail.com>
2024-09-18 21:41:16 -0300

    nascimento

---
+++ operadores/operacoes/op/src/nasci.java
@@ -0,0 +1,7 @@
+public class nasci {
+    public static void main(String[] args) throws Exception {
+       int idade = 39, anoAtual =2024;
+       int anoNascimento = anoAtual - idade;
+       System.out.println("O ano de nascimento é: " + anoNascimento);
+    }
+}
--- operadores/operacoes/op/src/ope.java
+++
@@ -1,7 +0,0 @@
-public class ope {
-    public static void main(String[] args) throws Exception {
-       int x = 10;
-       double y = x /3 ;
-       System.out.println("o resultado e: " + y); 
-    }
-}
